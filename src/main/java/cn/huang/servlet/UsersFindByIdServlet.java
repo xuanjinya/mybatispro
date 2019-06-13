2b18e9dd74dc86df925952acd2d13c089238591b
xuanjinya <1067998193@qq.com>
2019-06-13 19:58:38 +0800

    Log4j配置

--- src/main/java/cn/huang/servlet/UsersFindByIdServlet.java
+++ src/main/java/cn/huang/servlet/UsersFindByIdServlet.java
@@ -2,6 +2,7 @@ package cn.huang.servlet;
 
 import cn.huang.dao.UsersDAO;
 import cn.huang.entity.Users;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 /**
  * @Author: Yaking
  * @Date: 2019/6/12 19:35
@@ -17,6 +19,11 @@ import java.io.IOException;
  */
 @WebServlet("/detail")
 public class UsersFindByIdServlet extends HttpServlet {
+
+    /*创建对应的日志对象
+    * 通过不同的级别进行日志的记录【DEBUG,WARN,INFO,LOG】
+    * */
+    private Logger log = Logger.getLogger(UsersFindByIdServlet.class);
 
     private UsersDAO usersDAO = new UsersDAO();
 
@@ -28,7 +35,9 @@ public class UsersFindByIdServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
+        log.info("获取到查询参数ID："+id);
         Users user = usersDAO.findById(Integer.parseInt(id));
+        log.info("查询完成，查询的数据为："+user);
         req.setAttribute("user", user);
         req.getRequestDispatcher("detail.jsp").forward(req, resp);
     }
